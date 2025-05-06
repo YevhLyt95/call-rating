8a7aac6263414689b4c43ade40de642c684320d3
Hunter1995 <zenyalyt@gmail.com>
2025-05-06 22:04:33 +0200

    fixed routing

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,15 @@
 <header style="background: #ffffff; padding: :10px;">
     <nav>
-        <!-- Додати ще дропдаун та перед виходом пошук дзвінка -->
-        <a href="{{ route('index') }}">Мої дзвінки</a>
-        <a href="{{ route('my_calls') }}">Агреговані дзвінки</a>
-        <!--
-        <a href="{{ route('#') }}">Звіт по агентам</a>
-        <a href="{{ route('#') }}">Звіт Аналітика</a>
-        <a href="{{ route('#') }}">Адмінка</a>
-        <a href="#">Вийти</a> -->
+        
+        <a href="{{ route('index') }}">QA crm</a>
+        <a href="{{ route('my_calls') }}">Мої дзвінки</a>
+        {{-- 
+            <a href="{{ route('#') }}">Звіт по агентам</a>
+            <a href="{{ route('#') }}">Звіт Аналітика</a>
+            <a href="{{ route('#') }}">Адмінка</a>
+            <a href="#">Вийти</a>
+        
+        --}}
+        
     </nav>
 </header>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,55 @@
 @section('content')
 <div class="container">
     <h2>Деталі карти оцінки</h2>
-</div>
 
+    <table class="table table-bordered mt-3">
+        <tr>
+            <th>ID дзвінка/чату</th>
+            <td>{{ $card->call_id }}</td>
+        </tr>
+        <tr>
+            <th>SIP</th>
+            <td>{{ $card->sip }}</td>
+        </tr>
+        <tr>
+            <th>Тип карти</th>
+            <td>{{ $card->card_type }}</td>
+        </tr>
+        <tr>
+            <th>Мова</th>
+            <td>{{ $card->language }}</td>
+        </tr>
+        <tr>
+            <th>Дата дзвінка</th>
+            <td>{{ \Carbon\Carbon::parse($card->call_at)->format('d.m.Y') }}</td>
+        </tr>
+        <tr>
+            <th>Дата заповнення</th>
+            <td>{{ $card->created_at->format('d.m.Y') }}</td>
+        </tr>
+    </table>
+
+    <h4 class="mt-4">Оцінка:</h4>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Питання</th>
+                <th>Значення</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $content = json_decode($card->content, true);
+            @endphp
+            @foreach($content as $key => $value)
+                <tr>
+                    <td>{{ ucfirst(str_replace('_', ' ', $key)) }}</td>
+                    <td>{{ is_array($value) ? json_encode($value) : $value }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <a href="{{ route('my_calls') }}" class="btn btn-secondary mt-3">← Назад до списку</a>
+</div>
 @endsection
